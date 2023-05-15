90e6e314d3f25057a8ed465a134a82c58da37885
adrianhh1426 <adrianhh1426@gmail.com>
2023-05-15 17:54:32 -0600

    Agregando componente navbar

--- mf-navbar/src/components/Navbar.jsx
+++ mf-navbar/src/components/Navbar.jsx
@@ -2,9 +2,28 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar Component
-    </div>
+    <nav className="bg-gray-800">
+
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+              <img className="block h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </nav>
   );
 };
 
