a29a14068d66b50397879e0f0868e4b4e2bec8d9
adrianhh1426 <adrianhh1426@gmail.com>
2023-05-30 23:22:06 -0600

    agregando lazy loading

--- host-app/src/App.jsx
+++ host-app/src/App.jsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from "Navbar/Navbar";
 
 import "./index.scss";
-import HomePage from "./pages/HomePage";
-import PersonajesPage from "./pages/PersonajesPage";
-import AboutPage from "./pages/AboutPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import Loader from "./components/Loader";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const AboutPage = lazy(() => import("./pages/AboutPage"));
+const PersonajesPage = lazy(() => import("./pages/PersonajesPage"));
 
 const App = () => (
   <BrowserRouter>
     <Navbar />
     <div className="mx-20 mt-5">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/personajes" element={<PersonajesPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="*" element={<Navigate to='/' />} />
-        {/* <Route path="*" element={<NotFoundPage />} /> */}
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/personajes" element={<PersonajesPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<Navigate to='/' />} />
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
+        </Routes>
+      </Suspense>
     </div>
   </BrowserRouter>
 );
