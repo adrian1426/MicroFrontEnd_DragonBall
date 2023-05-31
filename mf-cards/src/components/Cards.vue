b8b9f4edf4ea51989f101884a408a984dde789ad
adrianhh1426 <adrianhh1426@gmail.com>
2023-05-31 12:03:03 -0600

    mf-cards - peticiones a API en vue

--- mf-cards/src/components/Cards.vue
+++ mf-cards/src/components/Cards.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1 class="text-center font-bold text-3xl">Cards Vue3 MF</h1>
+  <div>
+    <h1 class="text-center font-bold text-3xl">Rick and Morty</h1>
+
+    <div v-for="personaje in personajes" :key="personaje.id">
+      <img loading="lazy" width="150" height="150" :src="personaje.image" :alt="personaje.name"/>
+      {{personaje.name}}
+    </div>
+  </div>
 </template>
+
+<script setup>
+  import {onMounted,ref} from "vue";
+
+  const personajes = ref([]);
+
+  const getPersonajes = async()=>{
+    const respuesta = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await respuesta.json();
+
+    console.log(data.results)
+    personajes.value = data.results;
+  }
+
+  onMounted(()=>{
+    getPersonajes();
+  });
+</script>
