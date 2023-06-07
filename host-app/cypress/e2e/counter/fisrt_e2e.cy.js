4d82ef616731bab0a53cb8ae4b7e6a095cfd47cd
adrianhh1426 <adrianhh1426@gmail.com>
2023-06-07 15:33:30 -0600

    eventos - click

--- host-app/cypress/e2e/counter/fisrt_e2e.cy.js
+++ host-app/cypress/e2e/counter/fisrt_e2e.cy.js
@@ -5,4 +5,14 @@ describe("Counter tests", () => {
 
     cy.get("button").contains("+1");
   });
+
+  it("Debe Aumentar el counter", () => {
+    cy.visit("http://localhost:3000/");
+
+    cy.get("span").contains("20");
+    const btnAdd = cy.get("button").contains("+1");
+    btnAdd.click();
+
+    cy.get("span").contains("21");
+  });
 });
