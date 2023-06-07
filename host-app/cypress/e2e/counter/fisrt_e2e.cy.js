e618963b5f59c7aa5e559db45f7532950b96b6d3
adrianhh1426 <adrianhh1426@gmail.com>
2023-06-07 15:25:02 -0600

    accediendo a elementos del DOM

--- host-app/cypress/e2e/counter/fisrt_e2e.cy.js
+++ host-app/cypress/e2e/counter/fisrt_e2e.cy.js
@@ -1,5 +1,8 @@
 describe("Counter tests", () => {
   it("Debe Cargar el couter", () => {
     cy.visit("http://localhost:3000/");
+    cy.get("h1").contains("Counter App");
+
+    cy.get("button").contains("+1");
   });
 });
